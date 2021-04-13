ba34318d55949fe72765909ae2f5497660bed41a
Wolf721 <79043462+wolfwolf12@users.noreply.github.com>
2021-04-14 01:06:50 +0300

    Create ping.js

---
+++ commandgif/ping.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+const db = require('quick.db');
+
+exports.run = async (app, message, client) => {
+  
+const darknes = new Discord.MessageEmbed()
+
+.setColor("RANDOM")
+.setDescription('Ping')
+
+let start = Date.now(); 
+let mesaj = await message.channel.send(darknes)
+let diff = (Date.now() - start); 
+let API = (app.ws.ping).toFixed(2)
+    
+setInterval(() => {
+        
+const only = new Discord.MessageEmbed()
+
+.setDescription(`**${diff}ms**
+**${API}ms**`)
+
+mesaj.edit(only);
+      
+}, 5000)
+  
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'ping',
+  description: 'rexuss',
+  usage: 'ping'
+};
