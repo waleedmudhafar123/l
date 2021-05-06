bffebae3c51f2837103a6cc16f23e928396ac04d
Wolf721 <79043462+wolfwolf12@users.noreply.github.com>
2021-05-06 10:57:41 +0300

    Update wolf.js

--- wolf.js
+++ wolf.js
@@ -12,7 +12,7 @@ client.on('ready', async () => {
     client.appInfo = await client.fetchApplication();
   }, 600);
   
- client.user.setActivity(`=help`, { type:"LISTENING" })
+ client.user.setActivity(`${prefix}help`, { type:"LISTENING" })
   console.log("${client.user.tag}")
 });
 
