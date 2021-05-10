1b43bd57f3c6e3c7f961fa8189226c2c8e7a051c
Wolf721 <79043462+wolfwolf12@users.noreply.github.com>
2021-05-10 20:09:09 +0300

    Update gif-emoji.js

--- commandgif/gif-emoji.js
+++ commandgif/gif-emoji.js
@@ -8,7 +8,7 @@ let result = Math.floor((Math.random() * replies.length));
   
 let gifembed = new Discord.MessageEmbed()
 
-.setTitle("Emoji Gif")
+.setTitle("Emoji Gifs")
 .setColor("RANDOM")
 .setFooter(`Requested by ${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
 .setImage(replies[result]);
